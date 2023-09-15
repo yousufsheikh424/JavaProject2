f02a92a4b9d12830436ca94bd3a369a1d1aca7fd
yousufsheikh424 <137979547+yousufsheikh424@users.noreply.github.com>
2023-09-15 08:26:41 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 0247;
+        int b = 0X8579;
+        int c = 0b1101;
+        char m = 108;
+    String s = "Hello Java";
+
+    System.out.println("string literal : " +s);
+    System.out.println("Octa Literal : " +a);
+        System.out.println("Hexa Decimal Literal : " +b);
+        System.out.println("Binary Literal : " +c) ;
+        System.out.println("char as integral : " +m);
+        System.out.println("Hello team \nHow are you? \tI hope you're doing good");
+        System.out.println("\tI hope you're doing good");
+    }
+}
